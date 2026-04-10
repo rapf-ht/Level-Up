f47c8f01ad632080fbbb7b2494c43b9305057cc0
Rafael <javdt.rapf@gmail.com>
2026-04-09 22:13:22 -0300

    feat: changes in repo and styles for login page

--- src/components/signup.jsx
+++ src/components/signup.jsx
@@ -4,20 +4,32 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 export default function SignupForm() {
   return (
     <div className="container">
-      {
-        <form>
-        <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">Email</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-            <div id="emailHelp" class="form-text">Nós nunca compartilhamos seu email com ninguém</div>
+      <div className="col-12">
+        <div className="mb-3">
+          <label for="exampleInputEmail1" className="d-flex justify-content form-label">Email:</label>
+          <input 
+          type="email" 
+          className="form-control" 
+          id="exampleInputEmail1"
+          placeholder="Digite seu email:" 
+          aria-describedby="emailHelp" 
+          />
+          <div id="emailHelp" className="d-flex justify-content form-text">Nós nunca compartilhamos seu email com ninguém</div>
         </div>
-        <div class="mb-3">
-            <label for="exampleInputPassword1" class="form-label">Senha</label>
-            <input type="password" class="form-control" id="exampleInputPassword1" />
+        <div className="mb-3">
+            <label for="exampleInputPassword1" className="d-flex justify-content form-label">Senha:</label>
+            <input 
+            type="password" 
+            className="form-control"
+            placeholder="Digite sua senha:"
+            id="exampleInputPassword1" 
+            />
         </div>
-        <button type="submit" class="btn btn-primary">Login</button>
-        </form>
-      }
+        <div className= "col-12 d-flex justify-content-center gap-4">
+          <button type="submit" className="btn btn-primaryg">Login</button>
+          <button type="submit" className="btn btn-primary">Não tenho cadastro</button>
+        </div>
+      </div>
     </div>
   )
 }
