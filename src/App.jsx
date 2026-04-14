a304f036e72223660b39cbc0226ae2630e9a6619
Rafael <javdt.rapf@gmail.com>
2026-04-13 21:10:46 -0300

    feat(react-router): add react-router, add signup page, new route

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
-import { useState } from 'react'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import SigninForm from './components/signin'
+import SignupForm from './components/signup'
 
 function App() {
-  return <SigninForm />
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/signin" replace />} />
+        <Route path="/signin" element={<SigninForm />} />
+        <Route path="/signup" element={<SignupForm />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
--- src/components/signup.jsx
+++ src/components/signup.jsx
@@ -1,14 +1,13 @@
-import '../styles/signin.css'
+import '../styles/signup.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { Link } from 'react-router-dom'
 
-export default function SigninForm() {
+export default function SignupForm() {
   return (
-    <div className="container-fluid signin-wrapper">
+    <div className="container-fluid signup-wrapper">
       <div className="row justify-content-center align-items-center min-vh-100">
 
         <div className="col-11 col-sm-8 col-md-6 col-lg-4 col-xl-3">
-          <div className="signin-form">
+          <div className="signup-form">
 
             <h4 className="text-warning fw-bold text-center mb-4">
               🧙 Bem-vindo, Aventureiro(a)
@@ -23,20 +22,20 @@ export default function SigninForm() {
                 placeholder="Digite seu email"
                 aria-describedby="emailDisclaimer"
               />
-              <span id="emailDisclaimer" className="span-disclaimer">
+              <span id="emailDisclaimer" className="form-text span-disclaimer">
                 Nós nunca compartilhamos seu email com ninguém
               </span>
             </div>
 
             <div className="mb-4">
-              <label htmlFor="signin-password" className="form-label title-label">Senha:</label>
+              <label htmlFor="signup-password" className="form-label title-label">Senha:</label>
               <input
                 type="password"
                 className="form-control"
-                id="signin-password"
+                id="signup-password"
                 placeholder="Digite sua senha"
               />
-              <span className="span-disclaimer">
+              <span className="form-text span-disclaimer">
                 Mínimo 8 caracteres, letras maiúsculas e minúsculas
               </span>
             </div>
@@ -48,12 +47,12 @@ export default function SigninForm() {
             </div>
 
             <div className="text-center">
-              <Link to="/signup" className="icon-link icon-link-hover text-warning small">
+              <a className="icon-link icon-link-hover text-warning small" href="#">
                 Não tenho cadastro
                 <svg xmlns="http://www.w3.org/2000/svg" className="bi" width="16" height="16" viewBox="0 0 16 16" aria-hidden="true">
                   <path d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
                 </svg>
-              </Link>
+              </a>
             </div>
 
           </div>
