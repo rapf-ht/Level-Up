81fd34f9833e02e8607a56f01d353d45a1ccca5b
Rafael <javdt.rapf@gmail.com>
2026-05-20 01:40:34 -0300

    feat: redesign signin/signup

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -1,64 +1,60 @@
-import '../styles/signin.css'
+import styles from './Signin.module.css'
 import { Link } from 'react-router-dom'
 
 export default function SigninForm() {
   return (
-    <div className="container-fluid signin-wrapper">
-      <div className="row justify-content-center align-items-center min-vh-100">
+    <div className={styles.container}>
+      
+      <div className={styles['signin-form']}>
 
-        <div className="col-11 col-sm-8 col-md-6 col-lg-4 col-xl-3">
-          <div className="signin-form">
-
-            <h4 className="text-warning fw-bold text-center mb-4">
+            <h4 className={styles['title-intro']}>
               🧙 Bem-vindo, Aventureiro(a)
             </h4>
 
-            <div className="mb-3">
-              <label htmlFor="signin-email" className="form-label title-label">Email:</label>
+            <div className={styles['form-group']}>
+              <label htmlFor="signin-email" className={styles['form-label']}>Email:</label>
               <input
                 type="email"
-                className="form-control"
+                className={styles['form-control']}
                 id="signin-email"
-                placeholder="Digite seu email"
+                placeholder="  Digite seu email"
                 aria-describedby="emailDisclaimer"
               />
-              <span id="emailDisclaimer" className="span-disclaimer">
+              <span id="emailDisclaimer" className={styles['span-disclaimer']}>
                 Nós nunca compartilhamos seu email com ninguém
               </span>
             </div>
 
-            <div className="mb-4">
-              <label htmlFor="signin-password" className="form-label title-label">Senha:</label>
+            <div className={styles['form-group']}>
+              <label htmlFor="signin-password" className={styles['form-label']}>Senha:</label>
               <input
                 type="password"
-                className="form-control"
+                className={styles['form-control']}
                 id="signin-password"
-                placeholder="Digite sua senha"
+                placeholder="  Digite sua senha"
               />
-              <span className="span-disclaimer">
+              <span className={styles['span-disclaimer']}>
                 Mínimo 8 caracteres, letras maiúsculas e minúsculas
               </span>
             </div>
 
-            <div className="d-grid mb-3">
-              <Link to="/home" className="btn btn-warning fw-bold">
+            <div className={styles.grid}>
+              <Link to="/home" className={styles['btn-enter']}>
                 ⚔ Entrar
               </Link>
             </div>
 
-            <div className="text-center">
-              <Link to="/signup" className="icon-link icon-link-hover text-warning small">
+            <div className={styles['text-center']}>
+              <Link to="/signup" className={styles['btn-link']}>
                 Não tenho cadastro
-                <svg xmlns="http://www.w3.org/2000/svg" className="bi" width="16" height="16" viewBox="0 0 16 16" aria-hidden="true">
+                <svg xmlns="http://www.w3.org/2000/svg" className={styles['bi']} width="16" height="16" viewBox="0 0 16 16" aria-hidden="true">
                   <path d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
                 </svg>
               </Link>
             </div>
 
-          </div>
-        </div>
-
       </div>
+
     </div>
   )
 }
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,79 +1,81 @@
-import '../styles/signup.css'
+import styles from './Signup.module.css'
+import { Link } from 'react-router-dom'
 
 export default function SignupForm() {
   return (
-    <div className="container-fluid signup-wrapper">
-      <div className="row justify-content-center align-items-center min-vh-100">
+    <div className={styles.container}>
+      <div className={styles['signup-form']}>
 
-        <div className="col-11 col-sm-8 col-md-6 col-lg-4 col-xl-3">
-          <div className="signup-form">
-            
-            <div className="title-signup">
-              <h4 className="text-warning fw-bold text-center mt-2">
-                📜 Toda lenda começou de algum lugar...
-              </h4>
-              <h4 className="text-warning fw-bold text-center mb-3">
-                A sua começa aqui 📜
-              </h4>
-              <div className="choose-class">
-                <h6 className="text-choice mb-0">
-                  Escolha sua classe para começar a aventura:
-                </h6>
-              </div>
-              
-              <div className="d-flex justify-content-center gap-3 mt-2">
-                <button className="btn btn-outline-warning btn-sm btn-choice">Guerreiro ⚔️</button>
-                <button className="btn btn-outline-warning btn-sm btn-choice">Mago 🔮</button>
-                <button className="btn btn-outline-warning btn-sm btn-choice">Ladino 🏹</button>
-              </div>
-            
-            </div>
+        <h4 className={styles['title-intro']}>
+          📜 Toda lenda começou de algum lugar...
+        </h4>
+        <h4 className={styles['title-intro']}>
+          A sua começa aqui 📜
+        </h4>
 
-            <div className="mb-3">
-              <label htmlFor="signup-email" className="form-label title-label">Email:</label>
-              <input
-                type="email"
-                className="form-control"
-                id="signup-email"
-                placeholder="Digite seu email"
-                aria-describedby="emailDisclaimer"
-              />
-              <span id="emailDisclaimer" className="form-text span-disclaimer">
-                Nós nunca compartilhamos seu email com ninguém
-              </span>
-            </div>
-
-            <div className="mb-4 signup-passwords">
-              <label htmlFor="signup-password" className="form-label title-label">Criar senha:</label>
-              <input
-                type="password"
-                className="form-control"
-                id="signup-password"
-                placeholder="Digite sua senha"
-              />
-
-              <span className="form-text span-disclaimer">
-                Mínimo 8 caracteres, letras maiúsculas e minúsculas
-              </span>
-
-              <label htmlFor="signup-confirm-password" className="form-label title-label mt-3">
-                Confirmar Senha:
-              </label>
-              <input
-                type="password"
-                className="form-control"
-                id="signup-password"
-                placeholder="Digite sua senha novamente"
-              />
-            </div>
-
-            <div className="d-grid mb-3">
-              <button type="submit" className="btn btn-warning fw-bold">
-                ⚔ Começar sua jornada ⚔
-              </button>
-            </div>
-
+        <div className={styles['choose-class']}>
+          <h6 className={styles['text-choice']}>
+            Escolha sua classe para começar a aventura:
+          </h6>
+          <div className={styles['list-classes']}>
+            <button className={styles['class-btn']}>Guerreiro ⚔️</button>
+            <button className={styles['class-btn']}>Mago 🔮</button>
+            <button className={styles['class-btn']}>Ladino 🏹</button>
           </div>
+        </div>
+
+        <div className={styles['form-group']}>
+          <label htmlFor="signup-email" className={styles['form-label']}>Email:</label>
+          <input
+            type="email"
+            className={styles['form-control']}
+            id="signup-email"
+            placeholder="Digite seu email"
+            aria-describedby="emailDisclaimer"
+          />
+          <span id="emailDisclaimer" className={styles['span-disclaimer']}>
+            Nós nunca compartilhamos seu email com ninguém
+          </span>
+        </div>
+
+        <div className={styles['form-group']}>
+          <label htmlFor="signup-password" className={styles['form-label']}>Criar senha:</label>
+          <input
+            type="password"
+            className={styles['form-control']}
+            id="signup-password"
+            placeholder="Digite sua senha"
+          />
+          <span className={styles['span-disclaimer']}>
+            Mínimo 8 caracteres, letras maiúsculas e minúsculas
+          </span>
+        </div>
+
+        <div className={styles['form-group']}>
+          <label htmlFor="signup-confirm-password" className={styles['form-label']}>
+            Confirmar Senha:
+          </label>
+          <input
+            type="password"
+            className={styles['form-control']}
+            id="signup-confirm-password"
+            placeholder="Digite sua senha novamente"
+          />
+        </div>
+
+        <div className={styles['grid']}>
+          <button type="submit" className={styles['btn-enter']}>
+            ⚔ Começar sua jornada ⚔
+          </button>
+        </div>
+
+        <div className={styles['text-center']}>
+          <Link to="/signin" className={styles['btn-link']}>
+            Já tenho cadastro
+            <svg xmlns="http://www.w3.org/2000/svg" className={styles['bi']} width="16" height="16" viewBox="0 0 16 16" aria-hidden="true">
+              <path d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
+            </svg>
+          </Link>
         </div>
 
       </div>
