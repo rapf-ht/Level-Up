633e658af28161c8749d9689fc1adc24f601201b
Rafael <javdt.rapf@gmail.com>
2026-06-17 01:30:10 -0300

    feat: dtos for signin, signup

---
+++ backend/src/main/java/com/levelup/api/dtos/LoginDTO.java
@@ -0,0 +1,6 @@
+package com.levelup.api.dtos;
+
+public class LoginDTO {
+    public String email;
+    public String password;
+}
---
+++ backend/src/main/java/com/levelup/api/dtos/SignupDTO.java
@@ -0,0 +1,8 @@
+package com.levelup.api.dtos;
+
+public class SignupDTO {
+    public String user;
+    public String email;
+    public String password;
+    public String classPlayer; // String convertido para Enum depois
+}
